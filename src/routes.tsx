496e8367d8e3614d783453b9e42dd6b941705433
PixelBedrock <113065795+PixelBedrock@users.noreply.github.com>
2024-11-13 13:18:22 +0000

    feat: demo a second page and some lighthouse stuff

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { json, useLoaderData } from "react-router-dom";
+import { json, Link, useLoaderData } from "react-router-dom";
 
 type World = {
   message: string
@@ -13,7 +13,27 @@ export default [
     },
     Component() {
       let data: World = useLoaderData() as World;
-      return <h1>{data.message}</h1>;
+      return (
+        <>
+          <h1>{data.message}</h1>
+          <Link to={"/a-second-page"}>link to another page</Link>
+        </>
+      );
+    }
+  },
+  {
+    path: "/a-second-page",
+    loader() {
+      return json({ message: "it's a second page!" });
+    },
+    Component() {
+      let data: World = useLoaderData() as World;
+      return (
+        <>
+          <h1>{data.message}</h1>
+          <Link to={"/"}>link back to the first page</Link>
+        </>
+      );
     }
   }
 ];
--- src/server.tsx
+++ src/server.tsx
@@ -10,21 +10,30 @@ const hydrationBuilds = await Bun.build({
   define: {
     "process.env.NODE_ENV": JSON.stringify("production")
   },
-  minify: {
-    identifiers: true,
-    syntax: true,
-    whitespace: true
-  }
+  minify: true,
+  naming: "[name]-[hash].[ext]"
 });
 
 let handler = createStaticHandler(routes);
 
 Bun.serve({
   async fetch(req, _server) {
-    if (new URL(req.url).pathname === "/client.js")
-      return new Response(hydrationBuilds.outputs[0].stream(), {
+    if (new URL(req.url).pathname === hydrationBuilds.outputs[0].path.substring(1)) {
+      let uin8 = await Bun.readableStreamToBytes(hydrationBuilds.outputs[0].stream());
+      let gzip = Bun.gzipSync(uin8);
+      return new Response(gzip, {
         headers: {
+          "Cache-Control": "max-age=31536000",
+          "Content-Encoding": "gzip",
           "Content-Type": hydrationBuilds.outputs[0].type
+        }
+      });
+    }
+
+    if (new URL(req.url).pathname === "/robots.txt")
+      return new Response("User-Agent: *\nDisallow:", {
+        headers: {
+          "Content-Type": "text"
         }
       });
 
@@ -34,15 +43,18 @@ Bun.serve({
     let router = createStaticRouter(handler.dataRoutes, context);
 
     let stream = await renderToReadableStream(
-      <html>
+      <html lang="en">
         <head>
+          <title>SSR with React and react-router</title>
+          <meta name="description" content="SSR with React and react-router" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         </head>
         <body>
           <StaticRouterProvider router={router} context={context} />
         </body>
       </html>,
       {
-        bootstrapModules: ["./client.js"]
+        bootstrapModules: [hydrationBuilds.outputs[0].path.substring(1)]
       }
     );
 
